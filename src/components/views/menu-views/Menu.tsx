14c91480575a095b9722b828d59462670629b75e
KarolDawidG <karoldawidg@gmail.com>
2023-07-16 20:49:37 +0200

    Log 25: new footer has been added

--- src/components/views/menu-views/Menu.tsx
+++ src/components/views/menu-views/Menu.tsx
@@ -4,17 +4,17 @@ import { ImageMenu } from './ImageMenu';
 
 export const Menu = () => {
   return (
-    <div className="menu">
-      <div className="button-menu__options">
-        <Link to="/single">
-          <button className="game__reset-button">Single Player</button>
-        </Link>
-        <ImageMenu/>
-        <Link to="/multi">
-          <button className="game__reset-button">Multiplayer</button>
-        </Link>
+      <div className="menu">
+            <div className="button-menu__options">
+                  <Link to="/single">
+                    <button className="game__reset-button">Single Player</button>
+                  </Link>
+                  <Link to="/multi">
+                    <button className="game__reset-button">Multiplayer</button>
+                  </Link>
+                  <ImageMenu/>
+            </div>
       </div>
-    </div>
   );
 };
 
