124bda9f2e875ea97a5eb52f74eaaca9f0b178c8
KarolDawidG <karoldawidg@gmail.com>
2023-07-18 20:09:23 +0200

    Log 33: error occurred

--- src/components/utils/buttons/ResetPointsBtn.tsx
+++ src/components/utils/buttons/ResetPointsBtn.tsx
@@ -1,17 +1,29 @@
-import '../../css/button.css';
+import React, { useState } from 'react';
 
 export const ResetPointsBtn = () => {
+  const [resetKey, setResetKey] = useState(0);
+
   const handleClick = () => {
     localStorage.removeItem('pktG1');
     localStorage.removeItem('pktG2');
-    //window.location.reload();
+    setResetKey((prevKey) => prevKey + 1);
   };
 
-return (
-  <div className="menu">
-    <div className="button-menu__options">
-        <button className="game__reset-button" onClick={handleClick}>Resetuj punkty</button>     
+  const Refresh = () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 100);
+    return null;
+  };
+
+  return (
+    <div className="menu">
+      <div className="button-menu__options">
+        <button className="game__reset-button" onClick={handleClick}>Resetuj punkty</button>
+      </div>
+      {resetKey % 2 === 0 ? null : <Refresh />}
     </div>
-  </div>
-);
+  );
 };
+
+
