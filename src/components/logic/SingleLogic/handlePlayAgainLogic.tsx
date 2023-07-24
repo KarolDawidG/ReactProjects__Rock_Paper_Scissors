ad84b5b9729908793da444fe359b46292a02abba
KarolDawidG <karoldawidg@gmail.com>
2023-07-24 12:27:27 +0200

    Log 54: bugs to fixed

--- src/components/logic/SingleLogic/handlePlayAgainLogic.tsx
+++ src/components/logic/SingleLogic/handlePlayAgainLogic.tsx
@@ -1,4 +1,4 @@
-export const HandlePlayAgainLogic = (
+export const handlePlayAgainLogic = (
   setWynik: React.Dispatch<React.SetStateAction<string>>,
   setOpcjaNumber: React.Dispatch<React.SetStateAction<number | null>>,
   setR: React.Dispatch<React.SetStateAction<number | null>>,
--- src/components/views/single-views/DisplayResult.tsx
+++ src/components/views/single-views/DisplayResult.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import '../../css/game-views.css'
 import { WhatToDisplay } from './WhatToDisplay';
 import { Points } from '../../utils/other/Points';
-import { HandlePlayAgainLogic } from '../../logic/SingleLogic/HandlePlayAgainLogic';
+import { handlePlayAgainLogic } from '../../logic/SingleLogic/HandlePlayAgainLogic';
 import {Win} from "./Win";
 import {DisplayProps} from '../../utils/interfaces/DisplayProps';
 
 export const DisplayResult: React.FC<DisplayProps> = ({setWynik, setOpcjaNumber, setR, punkty, punktyPC, opcjaNumber, R, result, img}) => {
-  const handlePlayAgain = () => HandlePlayAgainLogic(setWynik, setOpcjaNumber, setR, punkty, punktyPC);
+  const handlePlayAgain = () => handlePlayAgainLogic(setWynik, setOpcjaNumber, setR, punkty, punktyPC);
 
     const renderMatch = (e: number, f:number) =>{
         if (e > 2) {
