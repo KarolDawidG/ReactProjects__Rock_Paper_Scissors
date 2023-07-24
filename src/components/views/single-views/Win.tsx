b1ebeb42e154f677031b16a06bf9121db523dee1
KarolDawidG <karoldawidg@gmail.com>
2023-07-24 18:15:21 +0200

    Log 58: some problems with refresh /single

--- src/components/views/single-views/Win.tsx
+++ src/components/views/single-views/Win.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import '../../css/win.css';
 import { WinProps } from '../../utils/interfaces/WinProps';
 import { Points } from '../../utils/other/Points';
-import { RefreshBtn } from '../../utils/buttons/RefreshBtn';
+import { RedirectBtn } from '../../utils/buttons/RedirectBtn';
 
 export const Win: React.FC<WinProps> = ({ whoWin, regards, punkty, punktyPC}) => {
   
   return (
     <div className={`win-main ${whoWin === 'PC' ? 'pc-win' : 'player-win'}`}>
-      <RefreshBtn/>
+      <RedirectBtn to="/single">Zagraj jeszcze raz</RedirectBtn>
       <h1 className='win_main-title'>Zwycięzca jest... {whoWin}</h1>
       <h2 className='win_title'>{regards}</h2>
       <Points punkty={punkty} punktyPC={punktyPC}/>
