72c3e6a9a88633fd94a8cf92d79fb266b6874e2d
KarolDawidG <karoldawidg@gmail.com>
2023-07-25 13:28:32 +0200

    Log 64: improvement of the algorithm performance

--- src/components/logic/SingleLogic/Algorithm.tsx
+++ src/components/logic/SingleLogic/Algorithm.tsx
@@ -1,18 +1,38 @@
-export const Algorithm = (playerChoices: number[]): number => { 
-   const choiceCounts: { [key: number]: number } = { 1: 0, 2: 0, 3: 0 };  //zlicza ilość  wystąpień wyborów
-        for (const choice of playerChoices) {       // Zlicz wystąpienia każdego wyboru w historii gracza
-        choiceCounts[choice]++;
-        }
-  
-    let mostFrequentChoice: number = 1;        // Znajdź najczęściej występujący wybór gracza
-    for (const choice in choiceCounts) {
-      if (choiceCounts[parseInt(choice)] > choiceCounts[mostFrequentChoice]) {
-        mostFrequentChoice = parseInt(choice);
-      }
+export const Algorithm = (playerChoices: number[]) => {
+    if (playerChoices.length < 3) {             // Sprawdź, czy mamy wystarczającą ilość danych, aby dokonać analizy
+      return Math.floor(Math.random() * 3) + 1;
     }
+  const choiceCounts: { [key: number]: number } = { 1: 0, 2: 0, 3: 0 };
+    for (const choice of playerChoices) {
+      choiceCounts[choice]++;
+    }
+    let mostFrequentChoice: number = 1;
+      for (const choice in choiceCounts) {
+        if (choiceCounts[choice] > choiceCounts[mostFrequentChoice]) {
+          mostFrequentChoice = parseInt(choice);
+        }
+      }
+       let estimatedChoice: number = (mostFrequentChoice % 3) + 1;
+          return estimatedChoice;
+};
+
+
+// zbyt trudna wersja, bo komputer od początku znał pierwszy wybór gracza, a więc pierwszą runde zawsze wygrywał
+// export const Algorithm = (playerChoices: number[]): number => { 
+//    const choiceCounts: { [key: number]: number } = { 1: 0, 2: 0, 3: 0 };  //zlicza ilość  wystąpień wyborów
+//         for (const choice of playerChoices) {       // Zlicz wystąpienia każdego wyboru w historii gracza
+//         choiceCounts[choice]++;
+//         }
   
-    let estimatedChoice: number = (mostFrequentChoice % 3) + 1;  // Szacuj kolejny wybór komputera 
+//     let mostFrequentChoice: number = 1;        // Znajdź najczęściej występujący wybór gracza
+//     for (const choice in choiceCounts) {
+//       if (choiceCounts[parseInt(choice)] > choiceCounts[mostFrequentChoice]) {
+//         mostFrequentChoice = parseInt(choice);
+//       }
+//     }
   
-    return estimatedChoice;
-  };
+//     let estimatedChoice: number = (mostFrequentChoice % 3) + 1;  // Szacuj kolejny wybór komputera 
+  
+//     return estimatedChoice;
+//   };
   
