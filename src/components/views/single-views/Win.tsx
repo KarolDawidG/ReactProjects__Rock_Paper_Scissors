cca95cb8da5965598c7fc336409a6619483b6b29
KarolDawidG <karoldawidg@gmail.com>
2023-07-25 19:58:22 +0200

    Log 68:repairing @media CSS styles

--- src/components/views/single-views/Win.tsx
+++ src/components/views/single-views/Win.tsx
@@ -8,18 +8,16 @@ export const Win: React.FC<WinProps> = ({ whoWin, regards, punkty, punktyPC }) =
   return (
     <div className={`win-main ${whoWin === 'ARTIFICIAL INTELLIGENCE' ? 'pc-win' : 'player-win'}`}>
       <div className="content-container">
-          <h1 className="win_main-title">The winner is {whoWin}</h1>
-            {whoWin === 'ARTIFICIAL INTELLIGENCE' && (
-              <img src="img/animeGirl.gif" alt="Manga Girl" className="animatet-girl" />
-            )}
-          <h2 className="win_title">{regards}</h2>
-            <div className='top-right'>
-                  <Points punkty={punkty} punktyPC={punktyPC}/>
-            </div>
-              <RefreshBtn />
+        <div className='top-right'>
+          <Points punkty={punkty} punktyPC={punktyPC} />
+        </div>
+        <h1 className="win_main-title">The winner is {whoWin}</h1>
+        {whoWin === 'ARTIFICIAL INTELLIGENCE' && (
+          <img src="img/animeGirl.gif" alt="Manga Girl" className="animatet-girl" />
+        )}
+        <h2 className="win_title">{regards}</h2>
+        <RefreshBtn />
       </div>
     </div>
   );
 };
-
-
