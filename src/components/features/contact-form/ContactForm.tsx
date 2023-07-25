c66e3d5483f405f699a1d1cf229a82681bad26cd
KarolDawidG <karoldawidg@gmail.com>
2023-07-25 17:52:07 +0200

    Log 66: contact-form validation

--- src/components/features/contact-form/ContactForm.tsx
+++ src/components/features/contact-form/ContactForm.tsx
@@ -1,38 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 import axios from 'axios';
 import { RedirectBtn } from '../../utils/buttons/RedirectBtn';
-import {CustomAlert} from "../custom-alert/CustomAlert";
+import { CustomAlert } from '../custom-alert/CustomAlert';
 import { showAlert, closeAlert } from '../custom-alert/CustomAlertUtils';
 
-export const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+interface FormState {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+export const ContactForm: React.FC = () => {
+  const [formState, setFormState] = useState<FormState>({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
   const [alert, setAlert] = useState({ message: '', type: '' });
 
-  const handleSubmit = async (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const formData = {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-    };
-
     try {
-      const response = await axios.post('https://rock-paper-scissors.ct8.pl/form', formData);
-      // const response = await axios.post('http://localhost:3001/form', formData);
-      
+      const response = await axios.post('https://rock-paper-scissors.ct8.pl/form', formState);
+      // const response = await axios.post('http://localhost:3001/form', formState);
 
       if (response.status === 200 && response.data === 'success') {
-
-        setName('');
-        setEmail('');
-        setSubject('');
-        setMessage('');
+        setFormState({ name: '', email: '', subject: '', message: '' });
         showAlert('An E-mail has been sent', 'success', setAlert);
       } else {
         console.log(response);
@@ -41,85 +44,108 @@ export const ContactForm = () => {
     } catch (error) {
       console.log(error);
       showAlert('Something went wrong! Check the console.', 'error', setAlert);
+    }
+  };
 
+  const backgroundColor = (value: number, e:number) =>{
+    return value >= e ? 'lightgreen' : 'lightgrey';
+  }
+
+  const preventSpace = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === ' ') {
+      e.preventDefault();
     }
   };
 
   return (
-   <>
-   <RedirectBtn to="/">Strona Główna</RedirectBtn>
-     <div className="form-container">
-      <form className="contact-form" onSubmit={handleSubmit}>
-        <h1 className="contact-form__title">CONTACT</h1>
+    <>
+      <RedirectBtn to="/">Strona Główna</RedirectBtn>
+      <div className="form-container">
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <h1 className="contact-form__title">CONTACT</h1>
 
-        <div className="contact-form__input-container">
-          <label htmlFor="name" className="contact-form__label">
-            Nick Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="contact-form__input"
-            minLength={4}
-            maxLength={12}
-            placeholder="Adam Smith"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
+          <div className="contact-form__input-container">
+            <label htmlFor="name" className="contact-form__label">
+              Nick Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="contact-form__input"
+              minLength={4}
+              maxLength={16}
+              placeholder="Adam Smith"
+              name="name"
+              value={formState.name}
+              onChange={handleChange}
+              style={{backgroundColor: `${backgroundColor(formState.name.length, 4)}`}}
+              onKeyDown={preventSpace}
+              required
+            />
+          </div>
 
-        <div className="contact-form__input-container">
-          <label htmlFor="email" className="contact-form__label">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="contact-form__input"
-            placeholder="example@mail.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
+          <div className="contact-form__input-container">
+            <label htmlFor="email" className="contact-form__label">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="contact-form__input"
+              placeholder="example@mail.com"
+              name="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+              style={{backgroundColor: `${formState.email.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/) ? 'lightgreen' : 'lightgrey'}`}}
+              value={formState.email}
+              onChange={handleChange}
+              onKeyDown={preventSpace}
+              required
+            />
+          </div>
 
-        <div className="contact-form__input-container">
-          <label htmlFor="subject" className="contact-form__label">
-            Subject
-          </label>
-          <input
-            type="text"
-            id="subject"
-            className="contact-form__input"
-            placeholder="Great projects!!!"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            required
-          />
-        </div>
+          <div className="contact-form__input-container">
+            <label htmlFor="subject" className="contact-form__label">
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              className="contact-form__input"
+              minLength={6}
+              maxLength={30}
+              style={{backgroundColor: `${backgroundColor(formState.subject.length, 6)}`}}
+              placeholder="Great projects!!!"
+              name="subject"
+              value={formState.subject}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="contact-form__input-container">
-          <label htmlFor="message" className="contact-form__label">
-            Message
-          </label>
-          <textarea
-            id="message"
-            className="contact-form__textarea"
-            cols={15}
-            rows={5}
-            placeholder="We invite you to cooperate!!!"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          ></textarea>
-        </div>
+          <div className="contact-form__input-container">
+            <label htmlFor="message" className="contact-form__label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              className="contact-form__textarea"
+              cols={15}
+              rows={5}
+              placeholder="We invite you to cooperate!!!"
+              name="message"
+              minLength={10}
+              maxLength={300}
+              style={{backgroundColor: `${backgroundColor(formState.message.length, 10)}`}}
+              value={formState.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
 
-        <input type="submit" className="contact-form__submit-btn" value="Send Message" />
-      </form>
-    </div>
-     {alert.message && <CustomAlert message={alert.message} type={alert.type} onClose={() => closeAlert(setAlert)} />}
-   </>
+          <input type="submit" className="contact-form__submit-btn" value="Send Message" />
+        </form>
+      </div>
+      {alert.message && <CustomAlert message={alert.message} type={alert.type} onClose={() => closeAlert(setAlert)} />}
+    </>
   );
 };
-
--- src/components/features/sentences/Sentenc.tsx
+++ src/components/features/sentences/Sentenc.tsx
@@ -4,7 +4,7 @@ export const Sentence = () => {
     const n:number = Math.floor(Math.random() * sentences.length);
     return(
         <>
-           <p style={{color: 'red', margin: '10px'}}>
+           <p style={{color: 'red', margin: '5px'}}>
                 {sentences[n]}
             </p> 
         </>
